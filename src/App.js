89a21d9bf1bbb8b4dd9bf683ea8fb658672808e6
Sevda <119746347+sevdakeskin@users.noreply.github.com>
2023-05-15 00:19:36 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Users from './components/Users';
+
+
+// Api url :   https://jsonplaceholder.typicode.com/users
+
+function App() {
+  return (
+    <div className="App">
+      <Users/>
+     
+    </div>
+  );
+}
+
+export default App;
